105d1d72973b86f581f8800b27fcd2994fe546ca
Kirandeep Kaur <89849204+kkauraiims@users.noreply.github.com>
2026-05-28 22:37:22 +0100

    Update and rename coregister_ft_kk.m to 01_ft_coregister_meg_mri.m

--- 01_ft_coregister_meg_mri.m.m
+++ 01_ft_coregister_meg_mri.m.m
@@ -20,6 +20,9 @@ function ft_coregister_meg_mri (patient_id)
 %     What is the anatomical label for the positive Y-axis [r, l, a, p, s, i]? a
 %     What is the anatomical label for the positive Z-axis [r, l, a, p, s, i]? s
 %     Is the origin of the coordinate system at the a(nterior commissure), i(nterauricular), n(ot a landmark)? i
+%   3. It is important to check the end alignment of the geometric data, misaligned geometry is likely to generate spurious sources
+%   4. The pre-processing for kurtosis differs from similar analytical procedures (e.g. computing Hurst exponent) with respect to bp filter.
+%   The lp for kurtosis in this function was kept high as 150Hz. 
 % Authors: CW, KK June 2022
 
 % specify data and output directories 
@@ -68,7 +71,73 @@ ft_plot_headshape(headshape);
 cd (patient_output_dir)
 save ('headshape');
 
+%% pre-process MEG 
 
+dataset = strcat (PFMEG(1).folder, '/', PFMEG(1).name);
 
+cfg = [];
+cfg.dataset = dataset;
+cfg.hpfilter = 'yes';
+cfg.hpfreq = 1; % can be changed to 10
+cfg.lpfilter = 'yes';
+cfg.lpfreq = 150;
+cfg.channel = {'megmag', 'meggrad'};
+cfg.coilaccuracy = 0;
+data = ft_preprocessing(cfg);
 
+% Resample data file to save memory space
+cfg = [];
+cfg.resamplefs = 500;
+data_resampled = ft_resampledata(cfg, data);
 
+%% compute data covariance window 
+cfg = [];
+cfg.channel = 'MEG';
+cfg.covariance = 'yes';
+cov_matrix = ft_timelockanalysis(cfg, data_resampled);
+
+cd (patient_output_dir);
+save ('cov_matrix');
+
+%% create headmodel 
+cfg = [];
+cfg.tissue = 'brain';
+cfg.spmversion = 'spm12';
+seg = ft_volumesegment(cfg, mri_realigned);
+
+save ('seg'); 
+
+cfg = [];
+cfg.tissue = 'brain';
+cfg.spmversion = 'spm12';
+brain_mesh = ft_prepare_mesh(cfg, seg);
+
+cfg = [];
+cfg.method = 'singleshell'; % previously was singleshell 
+cfg.grad = data_resampled.grad; 
+headmodel = ft_prepare_headmodel(cfg, brain_mesh);
+
+save ('headmodel')
+clear seg
+
+%% construction of source model 
+cfg = [];
+cfg.resolution = 10; % for clinical purpose should be 5mm or less
+cfg.unit = 'mm';
+cfg.headmodel = headmodel;
+cfg.grad = data_resampled.grad;
+sourcemodel = ft_prepare_sourcemodel(cfg);
+
+save ('sourcemodel') 
+
+%% plot all geometrical data to check their alignment
+figure
+ft_plot_axes([], 'unit', 'mm', 'coordsys', 'neuromag');
+ft_plot_headmodel(headmodel, 'unit', 'mm'); % this is the brain shaped head model volume
+ft_plot_sens(data_resampled.grad, 'unit', 'mm', 'coilsize', 10); % the sensor locations
+ft_plot_mesh(sourcemodel.pos, 'unit', 'mm'); % the source model is a cubic grid of points
+ft_plot_ortho(mri_realigned.anatomy, 'transform', mri_realigned.transform, 'style', 'intersect');
+alpha 0.5 % make the anatomical MRI slices a bit transparent
+
+savefig (gcf, 'alignment.fig', 'compact');
+close all
